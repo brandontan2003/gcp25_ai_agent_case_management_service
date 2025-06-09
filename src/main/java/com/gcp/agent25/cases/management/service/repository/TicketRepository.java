74972d331cdf42dfb09813c79f733cacc996860e
Brandon Tan <brandontan2003@gmail.com>
2025-06-09 11:09:58 +0800

    Optimize imports

--- src/main/java/com/gcp/agent25/cases/management/service/repository/TicketRepository.java
+++ src/main/java/com/gcp/agent25/cases/management/service/repository/TicketRepository.java
@@ -4,7 +4,6 @@ import com.gcp.agent25.cases.management.service.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
